70623fb1ee1b61c2dff3185372ae3329eac364d1
Paul <paul.turkovskiy@gmail.com>
2025-05-20 18:08:05 +0300

    refactor FastSellPlugin

--- src/main/java/pdf/anime/fastsellcmi/FastSellPlugin.java
+++ src/main/java/pdf/anime/fastsellcmi/FastSellPlugin.java
@@ -6,6 +6,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import pdf.anime.fastsellcmi.commands.FastSellCommand;
 import pdf.anime.fastsellcmi.config.ConfigContainer;
 import pdf.anime.fastsellcmi.listeners.FastSellMenuListener;
+import pdf.anime.fastsellcmi.services.MenuService;
+import pdf.anime.fastsellcmi.services.PDCService;
 import pdf.anime.fastsellcmi.utils.BukkitRunner;
 
 @Getter
@@ -14,12 +16,21 @@ public class FastSellPlugin extends JavaPlugin {
     private ConfigContainer configContainer;
     private PaperCommandManager commandManager;
 
+    private MenuService menuService;
+    private PDCService pdcService;
+    private BukkitRunner runnerService;
+
     @Override
     public void onEnable() {
-        configContainer = new ConfigContainer(getDataFolder());
-        configContainer.loadConfigs();
+        // Services
+        this.runnerService = new BukkitRunner(this);
+        this.pdcService = new PDCService(this);
+        this.configContainer = new ConfigContainer(getDataFolder());
+        this.configContainer.loadConfigs();
+        this.menuService = new MenuService(this.configContainer, this.pdcService);
 
-        getServer().getPluginManager().registerEvents(new FastSellMenuListener(configContainer, new BukkitRunner(this)), this);
+        // Listeners
+        getServer().getPluginManager().registerEvents(new FastSellMenuListener(this.configContainer, this.pdcService, this.runnerService), this);
 
         // Commands
         this.commandManager = new PaperCommandManager(this);
@@ -33,5 +44,6 @@ public class FastSellPlugin extends JavaPlugin {
         if (this.commandManager != null) {
             this.commandManager.unregisterCommands();
         }
+        getLogger().info("FastSellCMI disabled.");
     }
 }
