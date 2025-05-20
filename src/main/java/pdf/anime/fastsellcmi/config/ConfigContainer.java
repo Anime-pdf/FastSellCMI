14e1be006de21857d44e7c96a550bc5ed5d7ea01
Paul <paul.turkovskiy@gmail.com>
2025-05-20 17:58:48 +0300

    replace config holder/loader with advanced container

--- src/main/java/pdf/anime/fastsellcmi/config/ConfigContainer.java
+++ src/main/java/pdf/anime/fastsellcmi/config/ConfigContainer.java
@@ -1,8 +1,23 @@
 package pdf.anime.fastsellcmi.config;
 
 import lombok.Getter;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.spongepowered.configurate.CommentedConfigurationNode;
+import org.spongepowered.configurate.ConfigurateException;
+import org.spongepowered.configurate.objectmapping.ObjectMapper;
+import org.spongepowered.configurate.serialize.TypeSerializerCollection;
+import org.spongepowered.configurate.yaml.NodeStyle;
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
+import pdf.anime.fastsellcmi.config.serializers.*;
+import pdf.anime.fastsellcmi.utils.SoundContainer;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Getter
 public class ConfigContainer {
@@ -13,15 +28,75 @@ public class ConfigContainer {
 
     public ConfigContainer(File dataFolder) {
         this.dataFolder = dataFolder;
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs();
+        }
+    }
+
+    private <T> T loadConfiguration(Class<T> configClass, String fileName, TypeSerializerCollection serializers) {
+        File configFile = new File(dataFolder, fileName);
+        Path configPath = configFile.toPath();
+
+        YamlConfigurationLoader loader = YamlConfigurationLoader.builder()
+                .path(configPath)
+                .defaultOptions(opts -> opts.serializers(serializersInner -> serializersInner.registerAll(serializers)))
+                .nodeStyle(NodeStyle.BLOCK)
+                .indent(2)
+                .build();
+
+        try {
+            // Create file with default content if it doesn't exist
+            if (Files.notExists(configPath)) {
+                T newInstance = configClass.getDeclaredConstructor().newInstance();
+                CommentedConfigurationNode newNode = loader.createNode();
+                ObjectMapper.factory().get(configClass).save(newInstance, newNode);
+                loader.save(newNode);
+                return newInstance;
+            }
+
+            CommentedConfigurationNode node = loader.load();
+            T configInstance = ObjectMapper.factory().get(configClass).load(node);
+
+            ObjectMapper.factory().get(configClass).save(configInstance, node);
+            loader.save(node);
+
+            return configInstance;
+
+        } catch (ConfigurateException error) {
+            System.err.println("Error loading configuration: " + fileName);
+            System.err.println("Error: " + error.getMessage());
+            System.err.println("Error StackTrace: " + error.getMessage());
+            error.printStackTrace();
+            // Fallback: try to create a default instance if loading fails catastrophically
+            try {
+                System.err.println("Attempting to return a default instance for " + fileName);
+                return configClass.getDeclaredConstructor().newInstance();
+            } catch (ReflectiveOperationException roe) {
+                throw new RuntimeException("Failed to instantiate default config for " + fileName, roe);
+            }
+        } catch (ReflectiveOperationException e) { // For getDeclaredConstructor().newInstance()
+            throw new RuntimeException("Failed to create instance of config: " + fileName, e);
+        }
     }
 
     public void loadConfigs() {
-        languageConfig = new GeneralConfig(new File(dataFolder, "config.yml")).loadOrCreateConfig();
-        sellMenuConfig = new SellMenuConfig(new File(dataFolder, "sell_menu.yml")).loadOrCreateConfig();
+        TypeSerializerCollection generalSerializers = TypeSerializerCollection.builder()
+                .register(Component.class, new ComponentSerializer())
+                .register(SoundContainer.class, new SoundContainerSerializer())
+                .build();
+        languageConfig = loadConfiguration(GeneralConfig.class, "config.yml", generalSerializers);
+
+        TypeSerializerCollection sellMenuSerializers = TypeSerializerCollection.builder()
+                .register(Component.class, new ComponentSerializer())
+                .register(Color.class, new ColorSerializer())
+                .register(PotionEffect.class, new PotionEffectSerializer())
+                .register(FireworkEffect.class, new FireworkEffectSerializer())
+                .register(ItemStack.class, new SimpleItemStackSerializer())
+                .build();
+        sellMenuConfig = loadConfiguration(SellMenuConfig.class, "sell_menu.yml", sellMenuSerializers);
     }
 
     public void reloadConfigs() {
-        languageConfig.reload();
-        sellMenuConfig.reload();
+        loadConfigs();
     }
 }
